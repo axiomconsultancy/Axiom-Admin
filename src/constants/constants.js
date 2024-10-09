4a4724f3b6eca70600a5c6e98cb780a74c73f1e3
Attique Zaheer <mattiquezaheer@gmail.com>
2024-10-09 17:38:54 +0500

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,9 @@
 
 
 
-export const backendUrl = "http://localhost:5000";
+// export const backendUrl = "http://localhost:5000";
+export const backendUrl = "https://dx8ppwrc-5000.inc1.devtunnels.ms";
+
 
 
 // export const backendUrl = "https://4glkps52-5000.inc1.devtunnels.ms"
