e9e18bb5bc5999951b48190dee17e172c904a29a
Owais Mehmood <98080924+owais815@users.noreply.github.com>
2024-10-09 17:47:30 +0500

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,8 +3,8 @@
 
 
 // export const backendUrl = "http://localhost:5000";
+// export const backendUrl = "https://dx8ppwrc-5000.inc1.devtunnels.ms";
 export const backendUrl = "https://dx8ppwrc-5000.inc1.devtunnels.ms";
-
 
 
 // export const backendUrl = "https://4glkps52-5000.inc1.devtunnels.ms"
