6af2612d56b1cecfff6340f57ab2b4d5312756ed
Hammad Zamir <annibaba009@gmail.com>
2024-10-22 17:23:28 +0500

    reset passowrd functionality

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
-export const backendUrl = "http://localhost:5000";
-// export const backendUrl = "https://8rgvpdmw-5000.euw.devtunnels.ms";
+// export const backendUrl = "http://localhost:5000";
+export const backendUrl = "https://8rgvpdmw-5000.euw.devtunnels.ms";
 
 
 
